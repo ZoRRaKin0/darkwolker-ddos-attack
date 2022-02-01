0e6cac76285e07f2a6708a0db28287580443029c
Darkwolker <98810837+Darkwolker@users.noreply.github.com>
2022-02-01 13:55:54 +0300

    Update basla.sh

--- basla.sh
+++ basla.sh
@@ -1,8 +1,12 @@
+
 clear
 pkg install figlet
 clear
 figlet Python3 Modulu
 sleep 2
 pkg install python3
+pip3 install colorama
+clear
 figlet Devam...
-python3 darkwolker.py
+sleep 2
+python3 darkwolker.py 
